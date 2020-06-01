f2129dbc5c972313b0535c84ff95be0c26bbc639
noahShafer <noahshafer0211@gmail.com>
2020-06-01 12:17:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith'
+
+it('is set as Scuber', function () {
+    expect(companyName).to.equal('Scuber');
+  });
+
+  it('is set as Chelsea', function () {
+    expect(mostProfitableNeighborhood).to.equal('Scuber');
+  });
